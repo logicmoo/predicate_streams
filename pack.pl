682afae7cb9a651fdc1daabaf1c31d6b21f3e974
root <logicmoo@gmail.com>
2017-02-23 07:53:45 -0800

    v0.0.7

--- pack.pl
+++ pack.pl
@@ -2,10 +2,10 @@ name(predicate_streams).
 title('Implement your own Abstract Predicate Streams').
 
 version('0.0.7').
-download('https://github.com/TeamSPoon/pack_predicate_streams/releases/*.zip').
+download('https://github.com/TeamSPoon/predicate_streams/releases/*.zip').
 
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 maintainer( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
-home('https://github.com/TeamSPoon/pack_predicate_streams').
+home('https://github.com/TeamSPoon/predicate_streams').
 autoload(true).
