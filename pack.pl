c5ac0aa9a97b4b42c0777450c6dd984b33b04e41
root <logicmoo@gmail.com>
2017-03-13 06:52:50 -0700

    todo test the lambdas

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(predicate_streams).
 title('Implement your own Abstract Predicate Streams').
-version('1.1.112').
+version('1.1.113').
 download('https://github.com/TeamSPoon/predicate_streams/releases/*.zip').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
--- prolog/predicate_streams.pl
+++ prolog/predicate_streams.pl
@@ -1,5 +1,5 @@
 :- module(predicate_streams,
-          [ 
+          [
             with_input_from_predicate/2,     % +Pred1, +Goal
             with_output_to_predicate/2,      % +Pred1, +Goal
             with_error_to_predicate/2,       % +Pred1, +Goal
@@ -9,6 +9,7 @@
 
             is_predicate_stream/1,           % +Stream
             current_predicate_stream/1,      % ?Stream
+            close_hook/2,                     % ?Stream, +Goal
 
             set_current_input/1,             % +Stream
             set_current_output/1,            % +Stream
@@ -26,11 +27,11 @@
     E-mail:        logicmoo@gmail.com
     WWW:           http://www.logicmoo.org
     Copyright (C): 2015
-                       
+
     This program is free software; you can redistribute it and/or
     modify it.
 
-    18+ Years ago I remember these predicates existed as the building 
+    18+ Years ago I remember these predicates existed as the building
     blocks for Sockets in some Prolog I cannot remember.
 
 */
@@ -59,23 +60,26 @@ no_op(_).
 %
 :- dynamic(current_predicate_stream/1).
 :- volatile(current_predicate_stream/1).
+:- volatile(close_hook/2).
 
-% Hooks 
+% Hooks
 :- thread_local(stream_write/2).
 :- thread_local(stream_close/1).
 :- thread_local(stream_read/2).
 
- 	 	 
+:- thread_local(close_hook/2).
+
+
 :- meta_predicate(redo_cleanup_each(0,0,0)).
 redo_cleanup_each(Setup,Goal,Cleanup):-
-   \+ \+ '$sig_atomic'(Setup), 
-   catch( 
+   \+ \+ '$sig_atomic'(Setup),
+   catch(
      ((Goal, deterministic(DET)),
        '$sig_atomic'(Cleanup),
          (DET == true -> !
-          ; (true;('$sig_atomic'(Setup),fail)))), 
-      E, 
-      ('$sig_atomic'(Cleanup),throw(E))). 
+          ; (true;('$sig_atomic'(Setup),fail)))),
+      E,
+      ('$sig_atomic'(Cleanup),throw(E))).
 
 
 
@@ -88,13 +92,13 @@ current_output_is_user_output:- !,false.
 current_output_is_user_output:- stream_property(Stream,alias(user_output)),stream_property(Stream,alias(current_output)).
 
 % set current input stream and aliases
-set_current_input(In):- 
+set_current_input(In):-
  set_stream(In,  alias(current_input)),
  (current_input_is_user_input -> set_stream(In,  alias(user_input)) ; true),
  set_input(In).
 
 % set current output stream and aliases
-set_current_output(Out):- 
+set_current_output(Out):-
  set_stream(Out,  alias(current_output)),
  (current_output_is_user_output -> set_stream(Out,  alias(user_output)) ; true),
  set_output(Out).
@@ -103,11 +107,11 @@ set_current_output(Out):-
 % set_current_error(Err):- current_error_stream(ErrW)-> Err==ErrW,!.
 set_current_error(Err):-
  set_stream(Err, alias(current_error)),
- current_input(In), current_output(Out), 
+ current_input(In), current_output(Out),
  set_prolog_IO(In,Out,Err).
 
 % Get current error stream
-current_error_stream(Err):-   
+current_error_stream(Err):-
   stream_property(Err,alias(current_error))-> true;  % when we set it
   stream_property(Err,alias(user_error)) -> true;
   stream_property(Err,file_no(2)).
@@ -123,14 +127,14 @@ maybe_restore_input(Stream):-
   stream_property(Stream,alias(user_input)),
   original_input_stream(Was),!,
   set_current_input(Was).
-maybe_restore_input(_).   
+maybe_restore_input(_).
 
 :- volatile(original_input_stream/1).
 :- dynamic(original_input_stream/1).
 
 know_original_user_input:- ignore((\+ original_input_stream(_),
    stream_property(Was,alias(user_input)),
-   call(asserta,original_input_stream(Was)))).
+   asserta(original_input_stream(Was)))).
 
 :- if(( \+ source_file_property(reloading, true) , thread_self(M) , M == main )).
 :- stream_property(S,alias(user_output)),set_stream(S,alias(main_user_output)).
@@ -155,7 +159,7 @@ know_original_user_input:- ignore((\+ original_input_stream(_),
 %  ===
 
 with_output_to_predicate(Pred1,Goal):-
-   current_output(Prev),   
+   current_output(Prev),
    % stream_property(Prev,buffer_size(Size)),
    stream_property(Prev,buffer(Type)),
     with_predicate_output_stream(Pred1,Stream,
@@ -179,7 +183,7 @@ with_output_to_predicate(Pred1,Goal):-
 
 with_error_to_predicate(Pred1,Goal):-
    current_error_stream(Prev),
-    with_predicate_output_stream(Pred1,Stream, 
+    with_predicate_output_stream(Pred1,Stream,
        redo_cleanup_each(
           set_current_error(Stream),
           Goal,
@@ -200,7 +204,7 @@ with_error_to_predicate(Pred1,Goal):-
 
 with_input_from_predicate(Pred1,Goal):-
     current_input(Prev),
-    setup_call_cleanup(       
+    setup_call_cleanup(
        (new_predicate_input_stream(Pred1,Stream),
         set_stream(Stream, buffer_size(1))),
         redo_cleanup_each(set_current_input(Stream),Goal,set_current_input(Prev)),
@@ -213,7 +217,7 @@ with_input_from_predicate(Pred1,Goal):-
 %
 %  Checks to see if Stream was made by this API
 %
-is_predicate_stream(Stream):- 
+is_predicate_stream(Stream):-
    must_be(nonvar,Stream),
    current_predicate_stream(Stream).
 
@@ -223,7 +227,7 @@ is_predicate_stream(Stream):-
 %  Helper that creates and destroys (closes) a predicate output stream
 %
 with_predicate_output_stream(Pred1,Stream,Goal):-
-    setup_call_cleanup(       
+    setup_call_cleanup(
        new_predicate_output_stream(Pred1,Stream),
        Goal,
        whatevah(close(Stream))).
@@ -239,7 +243,7 @@ with_predicate_input_stream(Pred1,Stream,Goal):-
     setup_call_cleanup(
        new_predicate_output_stream(Pred1,Stream),
        Goal,
-       whatevah(close(Stream))). 
+       whatevah(close(Stream))).
 
 
 % =====================================================
@@ -250,16 +254,17 @@ with_predicate_input_stream(Pred1,Stream,Goal):-
 
 %! new_predicate_output_stream(:Pred1,-Stream)
 %
-%  Creates a new output stream that each write 
+%  Creates a new output stream that each write
 %  Invokes: call(+Pred1,+Data).
 
 new_predicate_output_stream(Pred1,Stream):-
   open_prolog_stream(predicate_streams, write, Stream, []),
-  call(asserta,(stream_write(Stream,Data):- whatevah(call(Pred1,Data))),Ref1),
-  call(asserta,current_predicate_stream(Stream),Ref2),
-  call(asserta,(
-    stream_close(Stream):- 
-    debug(predicate_streams,'~N% ~q.~n',[(stream_close(Stream):-flusing_output_to(Pred1))]),
+  asserta((stream_write(Stream,Data):- whatevah(call(Pred1,Data))),Ref1),
+  asserta(current_predicate_stream(Stream),Ref2),
+  asserta((
+    stream_close(Stream):-
+     debug(predicate_streams,'~N% ~q.~n',[(stream_close(Stream):-flusing_output_to(Pred1))]),
+       forall(retract(close_hook(Stream,Call)),ignore(Call)),
        whatevah(flush_output(Stream)),
        whatevah(erase(Ref1)),
        whatevah(erase(Ref2)),
@@ -269,18 +274,19 @@ new_predicate_output_stream(Pred1,Stream):-
 %
 %  Creates a new input stream that each read/getch()
 %  Invokes: call(+Pred1,-Data).
-%  
+%
 % @todo Discuss how to handle peek_char/2
 
 new_predicate_input_stream(Pred1,Stream):-
   open_prolog_stream(predicate_streams, read, Stream, []),
-  call(asserta,(stream_read(Stream,Data):- ignore(whatevah(call(Pred1,Data)))),Ref1),
-  call(asserta,current_predicate_stream(Stream),Ref2),
-  call(asserta,(
-    stream_close(Stream):-    
+  asserta((stream_read(Stream,Data):- ignore(whatevah(call(Pred1,Data)))),Ref1),
+  asserta(current_predicate_stream(Stream),Ref2),
+  asserta((
+    stream_close(Stream):-
     debug(predicate_streams,'~N% ~q.~n',[(stream_close(Stream):-call(Pred1,end_of_file))]),
+       forall(retract(close_hook(Stream,Call)),ignore(Call)),
        whatevah(erase(Ref1)),
-       whatevah(erase(Ref2)),       
+       whatevah(erase(Ref2)),
        retractall(stream_close(Stream)))).
 
 
@@ -291,7 +297,7 @@ new_predicate_input_stream(Pred1,Stream):-
 whatevah(Goal):- ignore(catch(Goal,error(_,_),fail)).
 
 % set_stream(Stream, buffer(line)), % useful?
-% set_stream(Stream, buffer_size(1)),   % useful? 
+% set_stream(Stream, buffer_size(1)),   % useful?
 % set_stream(Stream, close_on_exec(true)), % useful?
 % set_stream(Stream, close_on_abort(true)), % useful?
 
