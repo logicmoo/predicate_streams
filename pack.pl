8a5fc3991b8e0b2556df14b6ac59de999878a4e7
root <logicmoo@gmail.com>
2017-03-31 19:15:55 -0700

    rdf_rewrite:arity/2 2017-03-31-19:15:15

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(predicate_streams).
 title('Implement your own Abstract Predicate Streams').
-version('1.1.117').
+version('1.1.118').
 download('https://github.com/TeamSPoon/predicate_streams/releases/*.zip').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
