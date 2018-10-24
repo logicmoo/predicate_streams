ec8451507f6c73a9ea06952355aee63d638b26dc
Douglas R. Miles <logicmoo@gmail.com>
2018-10-24 12:29:29 +0000

    quieter

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(predicate_streams).
 title('Implement your own Abstract Predicate Streams').
 version('1.1.118').
-download('https://github.com/TeamSPoon/predicate_streams/releases/*.zip').
+download('https://github.com/TeamSPoon/predicate_streams.git/releases/*.zip').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
