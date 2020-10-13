f3f02672c1145a37622a50e04d7941e9594c39fc
logicmoo <logicmoo@gmail.com>
2020-10-13 03:52:40 -0700

    from root @ gitlab 2

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(predicate_streams).
 title('Implement your own Abstract Predicate Streams').
-version('2.0.2').
+version('2.0.3').
 download('https://github.com/TeamSPoon/predicate_streams/*').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
