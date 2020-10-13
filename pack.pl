24c144ee74ceb0da8587a4246aeb8cb476a9dc10
Douglas R. Miles <logicmoo@gmail.com>
2020-10-13 03:13:42 -0700

    hoping this fixes redirect warnings

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(predicate_streams).
 title('Implement your own Abstract Predicate Streams').
-version('2.0.1').
-download('http://github.com/TeamSPoon/predicate_streams/releases/*.zip').
+version('2.0.2').
+download('https://github.com/TeamSPoon/predicate_streams/releases/*.zip').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
-home('http://github.com/TeamSPoon/predicate_streams').
+home('https://github.com/TeamSPoon/predicate_streams').
 autoload(true).
